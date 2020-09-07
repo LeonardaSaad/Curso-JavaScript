7b2805e2e3290829e01c14dffbd39206ec89ee98
Leonarda Saad <leonardassaad777@gmail.com>
2020-09-07 16:55:34 -0300

    Módulo E - Aula 13 (Parte 1)

---
+++ AULA 13/ambiente.js
@@ -0,0 +1,26 @@
+var c = 1
+do {
+    console.log(`Fatia ${c}`)
+    c++
+} while (c <= 8)
+
+
+/*
+var c = 1
+while (c <= 8) {
+    console.log(`Fatia ${c}`)
+    c++
+}
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
