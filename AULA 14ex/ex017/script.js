7f5824ef64074eb5df9a11f9108373ac00b51b43
Leonarda Saad <leonardassaad777@gmail.com>
2020-09-13 15:48:15 -0300

    Módulo E - Aula Ex 17

--- AULA 14ex/ex017/script.js
+++ AULA 14ex/ex017/script.js
@@ -1,1 +1,16 @@
-alert('Olá!')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('select')
+    let n = Number(num.value)
+
+    if (num.value.length == 0) {
+        alert('Coloque um número')
+    } else {
+        tab.innerHTML = ''
+        for (c= 1;c<= 10;c+= 1) {
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            tab.appendChild(item)
+        }
+    }
+}
